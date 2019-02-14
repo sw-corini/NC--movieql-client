9f0e80eb9b78f294260e0fda44cafe6f825c7fc4
sw <genie_s2@naver.com>
2019-02-14 11:31:55 +0900

    end

--- src/Detail.js
+++ src/Detail.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Query } from "react-apollo";
+import { MOVIE_DETAILS } from "./queries";
 
 const Detail = ({
     match: {
         params: { movieId }
     }
-}) => {
-    console.log(movieId);
-    return <div>Detail</div>;
-};
+}) => (
+    <Query query={MOVIE_DETAILS} variables={{ movieId: movieId }}>
+        {({ loading, error, data }) => {
+            if (loading) return "loading";
+            if (error) return "error";
+            return (
+                <React.Fragment>
+                    {data.movie.title}
+                    {data.movie.medium_cover_image}
+                </React.Fragment>
+            );
+        }}
+    </Query>
+);
 
 export default Detail;
